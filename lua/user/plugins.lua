b5084507731db75f1d00a2296db02c3d2df63bc9
lavender010101 <lavender010101@163.com>
2022-06-10 22:54:22 +0800

    add jdtls

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -82,6 +82,12 @@ return require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
 
+  -- java
+  use 'mfussenegger/nvim-jdtls'
+
+
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
