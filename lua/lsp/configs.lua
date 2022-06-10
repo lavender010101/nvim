01e6140d79135aad3426be43a238d750e9da4bb4
lavender010101 <lavender010101@163.com>
2022-06-10 22:56:34 +0800

    add jsonls

--- lua/lsp/configs.lua
+++ lua/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = {"sumneko_lua", "jdtls"}
+local servers = {"sumneko_lua", "jsonls"}
 
 lsp_installer.setup {
 	ensure_installed = servers
