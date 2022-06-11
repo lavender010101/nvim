fa5fc41efbd134a56a4e3170b30193e45dbdcf9a
lavender010101 <lavender010101@163.com>
2022-06-11 12:00:23 +0800

    add code_runner()

---
+++ lua/user/functions.lua
@@ -0,0 +1,22 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+
+
+keymap("n", "r", ":lua code_runner()<CR>",opts)
+function code_runner()
+    vim.cmd "w"
+
+    if vim.o.filetype == 'lua' then
+       print(vim.o.filetype)
+
+    elseif vim.o.filetype == 'java' then
+        vim.cmd '!javac % && java %<'
+
+    elseif vim.o.filetype == 'python' then
+        vim.cmd '!python %'
+    end
+end
+
