d2cc0dd9d159de43f1301c9e090c383865caab5f
lavender010101 <lavender010101@163.com>
2023-07-09 03:02:10 +0800

    fix snippets support

--- lua/config/plugins/autocomplete.lua
+++ lua/config/plugins/autocomplete.lua
@@ -42,11 +42,16 @@ M.config = {
 				"rafamadriz/friendly-snippets",
 			},
 			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load({
+				-- Load snippets from my-snippets folder
+				-- If path is not specified, luasnip will look for the `snippets` directory
+				-- in rtp (for custom-snippet probably
+				-- `~/.config/nvim/snippets`).
+				require("luasnip.loaders.from_snipmate").load({
 					paths = {
 						"$HOME/.config/nvim/custom_snippets"
 					}
 				})
+				require("luasnip.loaders.from_snipmate").lazy_load() -- Lazy loading
 			end,
 		}
 
