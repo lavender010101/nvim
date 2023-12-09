2f444fbe7fcc1a53b55a023204b7834ff18cafd0
lavender010101 <lavender010101@163.com>
2023-12-09 14:50:45 +0800

    [feat] add rainbow_csv.nvim

--- lua/config/plugins/ft.lua
+++ lua/config/plugins/ft.lua
@@ -6,5 +6,25 @@ return {
 			vim.g.dart_corelib_highlight = false
 			vim.g.dart_format_on_save = false
 		end
+	},
+	{
+		'cameron-wags/rainbow_csv.nvim',
+		config = function()
+			require 'rainbow_csv'.setup()
+		end,
+		-- optional lazy-loading below
+		module = {
+			'rainbow_csv',
+			'rainbow_csv.fns'
+		},
+		ft = {
+			'csv',
+			'tsv',
+			'csv_semicolon',
+			'csv_whitespace',
+			'csv_pipe',
+			'rfc_csv',
+			'rfc_semicolon'
+		}
 	}
 }
