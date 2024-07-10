a702f80ff54eaca558ba2c95e2df1363eea28c4e
lavender010101 <lavender010101@163.com>
2024-07-10 08:31:28 +0800

    fix code_runner

--- lua/config/function.lua
+++ lua/config/function.lua
@@ -3,27 +3,23 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-
-
 keymap("n", "rr", ":lua code_runner()<CR>", opts)
 function code_runner()
-	vim.cmd "w"
+	vim.cmd("w")
 
-	vim.cmd "sp"
-	if vim.o.filetype == 'lua' then
+	vim.cmd("sp")
+	if vim.o.filetype == "lua" then
 		print(vim.o.filetype)
-	elseif vim.o.filetype == 'c' then
-		vim.cmd("term gcc % -o %<")
-		vim.cmd("term time ./%<")
-	elseif vim.o.filetype == 'cpp' then
-		vim.cmd("term g++ -std=c++11 % -Wall -o %<")
-		vim.cmd("term time ./%<")
-	elseif vim.o.filetype == 'java' then
-		vim.cmd 'res -5'
-		vim.cmd 'term javac % && time java %<'
-	elseif vim.o.filetype == 'python' then
-		vim.cmd 'term time python %'
-	elseif vim.o.filetype == 'sh' then
-		vim.cmd 'term time sh %'
+	elseif vim.o.filetype == "c" then
+		vim.cmd("term gcc % -o %< && time ./%<")
+	elseif vim.o.filetype == "cpp" then
+		vim.cmd("term g++ -std=c++11 % -Wall -o %< && time ./%<")
+	elseif vim.o.filetype == "java" then
+		vim.cmd("res -5")
+		vim.cmd("term javac % && time java %<")
+	elseif vim.o.filetype == "python" then
+		vim.cmd("term time python %")
+	elseif vim.o.filetype == "sh" then
+		vim.cmd("term time sh %")
 	end
 end
